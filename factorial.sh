9a89d4b355840a8f315e7ba43aceb6db38c7eff3
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:31:16 +0530

    Update factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -9,3 +9,14 @@ do
 	factor=$(($factor*$i))
 done
 echo "Factorial of $num is $factor"
+
+Algorithm
+
+1. Input a number and store it in num.
+2. Set factor = 1
+3. If num is less than 0, then:
+   a. Print "not defined"
+   b. Exit with a status code of 1
+4. For i from 1 to num (inclusive), do:
+   a. Set factor = factor * i
+5. Print Factorial 
