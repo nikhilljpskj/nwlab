8ec7859ad097623e3051e6f3ddc95f077468ceaf
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:33:20 +0530

    Update leap.sh

--- leap.sh
+++ leap.sh
@@ -11,3 +11,15 @@ then
 	else
 	echo "$year is not a leap year"
 fi 
+
+
+Algorithm
+
+1. Input a year and store it in year.
+2. If year is divisible by 4, then:
+   a. If year is divisible by 400 or not divisible by 100, then:
+      - Print "year is a leap year."
+   b. Else:
+      - Print "year is not a leap year."
+3. Else:
+   - Print "year is not a leap year."
