2560ac46a327473289d8878ef250cfb3a3aeb294
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:32:27 +0530

    Update fibo.sh

--- fibo.sh
+++ fibo.sh
@@ -21,3 +21,23 @@ secondNum=1
 		secondNum=$thirdNum
 	done
 fi
+
+
+Algorithm
+
+1. Input a range and store it in finalNo.
+2. Set firstNum = 0 and secondNum = 1.
+3. If finalNo equals 0, then:
+   a. Print firstNum.
+4. Else, if finalNo equals 1, then:
+   a. Print firstNum.
+   b. Print secondNum.
+5. Else:
+   a. Print firstNum.
+   b. Print secondNum.
+   c. For i from 2 to finalNo - 1:
+      i. Calculate thirdNum = firstNum + secondNum.
+      ii. Print thirdNum.
+      iii. Update firstNum = secondNum.
+      iv. Update secondNum = thirdNum.
+6. End the loop when i reaches finalNo - 1.
