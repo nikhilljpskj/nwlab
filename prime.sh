0250af4881bcf1abd707b017da8d444c7be3a438
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:34:14 +0530

    Update prime.sh

--- prime.sh
+++ prime.sh
@@ -18,3 +18,18 @@ then
 else
 	echo "$num is a not prime number"
 fi
+
+
+Algorithm
+
+1. Input a number and store it in num.
+2. Set count = 0
+3. For i from 1 to num:
+   a. If num equals 1:
+      - Break out of the loop (not a prime number).
+   b. If num is divisible by i (num % i equals 0):
+      - Increment count.
+4. If count equals 2:
+   - Print "num is a prime number."
+   - Else:
+   - Print "num is not a prime number."
