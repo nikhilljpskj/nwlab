0907d996ac983b879177017c2e482bcaf700c226
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:38:16 +0530

    Update upptolower.sh

--- upptolower.sh
+++ upptolower.sh
@@ -28,3 +28,28 @@ echo "The file sorted in desending order"
 
 echo "$(sort -r "Temp")"
 rm Temp
+
+
+Algorithm
+
+1. Input a file name and store it in fileName.
+2. If the file with the name fileName does not exist, then:
+   a. Print "File not found."
+   b. Exit the script with an error code.
+3. If the file exists, then:
+   a. Print "Converting from lower case to uppercase."
+   b. Convert the file content from lowercase to uppercase using tr and display it.
+   c. Print an empty line for separation.
+   d. Print "Converting from Uppercase to lowercase."
+   e. Convert the file content from uppercase to lowercase using tr and display it.
+   f. Print an empty line for separation.
+   g. Print "The number of lines is:" followed by the line count of the file using wc.
+   h. Print "The number of words is:" followed by the word count of the file using wc.
+   i. Calculate the number of characters in the file using wc and store it in char.
+   j. Print "The number of characters is:" followed by the value of char.
+4. Create a temporary file named "Temp."
+5. Iterate through each word in the original file:
+   a. Append each word to the "Temp" file.
+6. Print "The file sorted in descending order."
+   a. Sort the "Temp" file in reverse order using sort and display the result.
+7. Remove the "Temp" file.
