6150a5581c0adfc2dbedd0584ec52dd6e15eab91
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:36:06 +0530

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -13,3 +13,15 @@ done
 
 
 echo "Reversed number: $reverse"
+
+
+Algorithm
+
+1. Prompt the user to enter a number and store it in number.
+2. Initialize reverse to 0.
+3. While number > 0:
+   a. Calculate remainder = number % 10.
+   b. Multiply reverse by 10 and add remainder to it.
+   c. Update number by removing its last digit (integer division by 10).
+4. The value in reverse is the reversed number.
+5. Print the reversed number.
