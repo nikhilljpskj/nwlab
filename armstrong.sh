5bdec56289c58ad36e1dbe7782fadd1094d2c326
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2023-09-11 09:29:15 +0530

    Update armstrong.sh

--- armstrong.sh
+++ armstrong.sh
@@ -15,3 +15,20 @@ then
 else
 	echo "The number $num is not a armstrong number"
 fi
+
+
+Algorithm
+
+1. Input a number and store it in num.
+2. Set temp = num
+3. Calculate the length of num and store it in length.
+4. Set armstrong = 0
+5. While temp > 0:
+   a. Get the last digit of temp and store it in digit.
+   b. Calculate value = digit^length.
+   c. Add value to armstrong.
+   d. Update temp by removing its last digit.
+6. If armstrong equals num, then:
+   a. Print "The number num is an Armstrong number."
+   b. Else:
+   c. Print "The number num is not an Armstrong number."
